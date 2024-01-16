bfedbf4f06d0f0c6f59fa9e8c94d122ec356e333
RickCraco <riccardocracolici43@gmail.com>
2024-01-16 16:21:54 +0100

    added routing to index projects

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,12 +2,13 @@
 @section('content')
     <section class="container my-4">
         <h1 class="text-danger">{{ $project->title }}</h1>
-        <div class="card w-50 bg-dark text-white border-white">
+        <div class="card w-50 bg-dark text-white border-white mt-5">
             <img src="{{asset('storage/' . $project->img) }}" class="card-img-top" alt="{{ $project->title }}">
             <div class="card-body">
                 <h5 class="card-title">{{ $project->title }}</h5>
                 <p class="card-text">{{ $project->body }}</p>
                 <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-danger">Edit</a>
+                <a href="{{ route('admin.projects.index') }}" class="btn btn-danger">Back</a>
             </div>
         </div>
     </section>
