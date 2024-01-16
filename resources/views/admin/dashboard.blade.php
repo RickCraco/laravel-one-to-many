71da4e497c1966426e58a74be80d363b85fd8ce8
RickCraco <riccardocracolici43@gmail.com>
2024-01-16 16:12:18 +0100

    added style to dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card bg-dark text-white border-white">
-                <div class="card-header border-white">{{ __('User Dashboard') }}</div>
+                <div class="card-header border-white">{{ __('Admin Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -17,9 +17,12 @@
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    You are logged in as {{ Auth::user()->name }}.
                     <div class="mt-3">
+                        <div class="my-4">If you want to see all your projects click below <i class="fa-solid fa-arrow-down"></i></div>
                         <a href="{{ route('admin.projects.index') }}" class="btn btn-danger">Control Panel</a>
+                        <div class="my-4">If you want to see all your categories click here <i class="fa-solid fa-arrow-down"></i></div>
+                        <a href="{{ route('admin.categories.index') }}" class="btn btn-danger">Control Panel</a>
                     </div>
                 </div>
             </div>
