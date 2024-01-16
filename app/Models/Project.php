a50a3067934257402853af4821cb758367dcc532
RickCraco <riccardocracolici43@gmail.com>
2024-01-16 11:04:58 +0100

    added relation one to many user projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,4 +9,9 @@ class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'title', 'slug', 'body', 'image'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
